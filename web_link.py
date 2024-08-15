a88b2e4f54f3f8761224f3e4486bb53ea5b30c8e
Engine55 <haxu4@student.unimelb.edu.au>
2024-08-15 16:53:31 +0800

    improve code style and give more meaningful name to variables

--- web_link.py
+++ web_link.py
@@ -8,71 +8,45 @@ import requests
 from queue import Queue
 from bs4 import BeautifulSoup
 
-
 broken_links = list()
 def scrape_pages(baseurl):
     # the file used to record the broken links and its parent links
     with open('broken_links.txt', 'w') as f:
-
-        visited_or_about_to_visit = set()
-
-        to_visit_list = Queue()
+        captured_links = set()
+        to_visit = Queue()
         # base url don't have parent in pattern [current link, source page link]
-        to_visit_list.put([baseurl,None])
-
-
-
-        while to_visit_list:
-
-            url_combo = to_visit_list.get()
+        to_visit.put([baseurl, None])
+        while to_visit:
+            url_combo = to_visit.get()
             url = url_combo[0]
-
-
-
             try:
-
                 response = requests.get(url)
-                visited_or_about_to_visit.add(url)
-
-
+                captured_links.add(url)
                 if response.status_code == 200:
                     # for external links just need to check its accessibility, no need to find sublink
                     if not url.startswith(baseurl):
                         continue
-
                     soup = BeautifulSoup(response.content, 'html.parser')
-
-
-
                     # Print the current URL
-
                     print(url)
-
-
-
                     # Find all links on the page
-
                     for link in soup.find_all('a', href=True):
-
                         href = link['href']
                         # Check if the link already in the list to avoid repeat
-
-                        if href not in visited_or_about_to_visit:
-
-                            visited_or_about_to_visit.add(href)
-                            to_visit_list.put([href,url])
-
+                        if href not in captured_links:
+                            captured_links.add(href)
+                            to_visit.put([href, url])
                             print('new links founded', href)
                 else:
                     print(f'broken link{url} founded with status {response.status_code}')
                     print()
                     broken_links.append(url)
-                    f.write('broken link is ' + url + '  page source url is ' + url_combo[1] + '  status_code: ' + str(response.status_code) + '\n')
+                    f.write('broken link is ' + url + '  page source url is ' + url_combo[1] + '  status_code: ' + str(
+                        response.status_code) + '\n')
             except requests.exceptions.RequestException as e:
                 print(f"Error fetching {url}: {e}")
 
+
 # Replace with the base URL you want to scrape
-
 base_url = 'https://sites.research.unimelb.edu.au/research-funding'
-
 scrape_pages(base_url)
--- web_specified_text_link.py
+++ web_specified_text_link.py
@@ -4,64 +4,36 @@
 import requests
 from queue import Queue
 from bs4 import BeautifulSoup
-
-
 def scrape_pages(baseurl, search_string):
     # the file used to record the broken links and its parent links
     with open('string_pattern_links.txt', 'w') as f:
-
         visited = set()
-        to_visit_list = Queue()
-        to_visit_list.put(baseurl)
-
-
-
-        while not to_visit_list.empty():
-            url = to_visit_list.get()
-            if url in visited or url in to_visit_list:
+        to_visit = Queue()
+        to_visit.put(baseurl)
+        while not to_visit.empty():
+            url = to_visit.get()
+            if url in visited or url in to_visit:
                 continue
             try:
-
                 response = requests.get(url)
-
                 visited.add(url)
-
-
                 if response.status_code == 200:
                     soup = BeautifulSoup(response.content, 'html.parser')
-
-
-
-                    # Print the current URL
-
                     print(url)
                     page_text = soup.get_text()
-
                     if search_string in page_text:
                         print("The specified string was found in the page source.", url)
                         f.write(url + '\n')
                     # Find all links on the page
-
                     for link in soup.find_all('a', href=True):
-
                         href = link['href']
-
-
-
                         # Check if the link already in the list to avoid repeat
                         if href.startswith(baseurl):
-                            if href not in to_visit_list and href not in visited:
-                                to_visit_list.put(href)
-
+                            if href not in to_visit and href not in visited:
+                                to_visit.put(href)
                                 print('new links founded', href)
             # i guess no need to handle with error here? as only task is to search specified text
             except Exception as e:
                 pass
-
-
-
-# Replace with the base URL you want to scrape
-
 base_url = 'https://sites.research.unimelb.edu.au/research-funding'
-
 scrape_pages(base_url, search_string = "Funding Partners")
