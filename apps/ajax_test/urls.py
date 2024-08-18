05e764dbc6f7b17a7f20c77b94371866c12f4dc0
Zhonghao <zhosheng@student.unimelb.edu.au>
2024-08-19 03:44:30 +1000

    Overall structure refined, integrate broken link searching functionality into web app, simple result and search pages implemented for testing purpose

--- apps/ajax_test/urls.py
+++
@@ -1,7 +0,0 @@
-from ajax_test.views import index,product
-from django.urls import path
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('pro/', product, name='pro')
-]
--- website_review_tool/urls.py
+++ website_review_tool/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ajax_test.urls')),
+    path('', include('search_link.urls')),
     path('login/', include('login.urls')),
 ]
