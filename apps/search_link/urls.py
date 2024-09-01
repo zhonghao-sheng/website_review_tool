61bf82fff8834cb666b70496abf803deb20f8a11
Zhonghao <zhosheng@student.unimelb.edu.au>
2024-09-01 18:33:24 +1000

    Debugging task assigning function

--- apps/search_link/urls.py
+++ apps/search_link/urls.py
@@ -4,4 +4,5 @@ from search_link import views
 urlpatterns = [
     # path('', views.search, name='search'),
     path('search_link/', views.search_link, name='search_link'),  # Search page
+    path('results/', views.results_view, name='results'),
 ]
