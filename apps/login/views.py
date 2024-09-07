920e5935e1f26d26d09d8498ba5a148057c4d83f
Engine55 <haxu4@student.unimelb.edu.au>
2024-09-07 08:52:57 +0800

    attempt email activation

--- apps/login/views.py
+++ apps/login/views.py
@@ -12,7 +12,13 @@ from django.conf import settings
 from django.shortcuts import redirect, render, redirect
 from django.urls import reverse
 from urllib.parse import quote_plus, urlencode
-
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.core.mail import EmailMessage
+#from .tokens import account_activation_token
+import requests
 oauth = OAuth()
 
 oauth.register(
@@ -26,12 +32,12 @@ oauth.register(
 )
 
 
-
 def login_user(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
+            activate_email(request, user, form.cleaned_data.get('email'))
             login(request, user)
             messages.success(request, 'You are now logged in.')
             return oauth.auth0.authorize_redirect(
@@ -43,11 +49,15 @@ def login_user(request):
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
+
+
 def callback(request):
     token = oauth.auth0.authorize_access_token(request)
     request.session["user"] = token
     print("callback function invoked")
     return redirect(request.build_absolute_uri(reverse("index")))
+
+
 def logout_user(request):
     logout(request)
     messages.success(request, 'You have been logged out.')
@@ -67,7 +77,6 @@ def logout_user(request):
 
 
 def index(request):
-
     return render(
         request,
         "index.html",
@@ -78,21 +87,78 @@ def index(request):
     )
 
 
+def activate(request, uidb64, token):
+    '''
+    model = user
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = model.objects.get(pk=uid)
+    except:
+        user = None
+
+    if user is not None: #and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, f"Email has been confirmed. Now you can log into your account.")
+        return redirect('login')
+    else:
+        messages.error(request, f"Link is invalid!")
+    '''
+
+    messages.success(request, f"This function works!")
+    return redirect('index')
+
+
+def activate_email(request, user, email):
+    email = "jazheng@student.unimelb.edu.au"
+    subject = "Activate your account."
+    message = render_to_string("activate.html", {
+        'user': user.username,
+        'domain': get_current_site(request).domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        # 'token': account_activation_token.make_token(user),
+        'protocol': 'https' if request.is_secure() else 'http'
+    })
+    email = EmailMessage(subject, message, to=[email])
+    if email.send():
+        messages.success(request, f"Thank you {user} for signing up to the website review tool. Please check\
+                     your email {email} inbox and click on the activation link to confirm registration.")
+    else:
+        messages.error(request, f"Problem sending email to {email}. Please ensure you have typed it correctly.")
+
+
+# def signup(request):
+#     if request.method == 'POST':
+#         form = SignUpForm(request.POST)
+#         if form.is_valid():
+#             user = form.save(commit=False)
+#             user.is_active = False
+#             user.save()
+#             activate_email(request, user, form.cleaned_data.get('email'))
+#             return redirect('index')  # Redirect to home page after successful signup
+#     else:
+#         form = SignUpForm()
+#     return render(request, 'signup.html', {'form': form})
+
+
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
+        email = "haxu4@student.unimelb.edu.au"
         if form.is_valid():
             user = form.save()
             user.refresh_from_db()  # Load the profile instance created by the signal
             user.save()
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
+            activate_email(request, user, form.cleaned_data.get('email'))
             user = authenticate(username=username, password=password)
             login(request, user)
             return redirect('index')  # Redirect to home page after successful signup
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
+
 
 def check_login(request):
     if request.user.is_authenticated:
---
+++ web_search.py
@@ -0,0 +1,168 @@
+from django.shortcuts import render
+from multiprocessing import JoinableQueue as Queue
+from threading import Thread
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# import time
+import requests
+from bs4 import BeautifulSoup
+class Web_spider():
+    def __init__(self):
+        self.visited_or_about_to_visit = set()
+        self.web_links = Queue()
+        self.baseurl = 'https://sites.research.unimelb.edu.au/research-funding'
+        self.broken_link_file = open('broken_links_faster.txt', 'w')
+        self.UOM_sign_links = list()
+        self.uom_sign_link_file = open('uom_links.txt','w')
+        self.keyword_link_file = open('keyword_links.txt','w')
+        self.counter = 0
+        self.broken_links = list()
+        self.keyword = 'Funding Partners'
+        self.keyword_links = list()
+
+
+    def put_url(self, baseurl):
+        # [link, source page link, associated text]
+        self.web_links.put([baseurl, None, None])
+        self.counter += 1
+        self.baseurl = baseurl
+    def put_keyword(self, keyword):
+        self.keyword = keyword
+    def is_uom_sign_link(self, link):
+        return self.baseurl in link
+    def add_uom_sign_link(self, link, source_link):
+        self.UOM_sign_links.append({'url': link, 'source_link': source_link})
+
+    def write_uom_sign_link(self, link, source_link):
+        self.uom_sign_link_file.write(f'uom link:{link}, page source:{source_link}\n')
+
+    def deal_uom_sign_link(self, link, source_link):
+        if self.is_uom_sign_link(link):
+            self.add_uom_sign_link(link, source_link)
+            self.write_uom_sign_link(link, source_link)
+    def add_broken_link(self, link, source_link, associated_text):
+        self.broken_links.append({'url': link, 'source_link':
+            source_link, 'associated_text': associated_text})
+
+    def write_broken_link(self, link, source_link, response_status, associated_text):
+        self.broken_link_file.write(f'status:{response_status}, broken link: '
+                                    f'{link}, page source: {source_link}, associated_text: {associated_text}\n')
+
+    def deal_broken_link(self, link, source_link, response_status, associated_text):
+        self.add_broken_link(link, source_link, associated_text)
+        self.write_broken_link(link, source_link, response_status, associated_text)
+
+    def get_more_links(self):
+        while True:
+            link_combo = self.web_links.get()
+
+            link = link_combo[0]
+            try:
+                print(f'getting link {link}')
+                response = requests.get(link)
+                self.visited_or_about_to_visit.add(link)
+                if response.status_code == 200:
+                    if not link.startswith(self.baseurl):
+                        continue
+                    soup = BeautifulSoup(response.content, 'html.parser')
+                    if self.keyword is not None:
+                        text = soup.get_text()
+                        if self.keyword in text:
+                            print(f'found keyword {self.keyword} in link {link}')
+                            self.keyword_links.append(link)
+                            self.keyword_link_file.write(link + '\n')
+                    for href_link in soup.find_all('a', href=True):
+                        href = href_link['href']
+                        text = href_link.get_text()
+                        if href not in self.visited_or_about_to_visit:
+                            if 'mailto:' not in href:
+                                self.visited_or_about_to_visit.add(href)
+                                self.web_links.put([href, link, text])
+                                self.counter += 1
+                            else:
+                                print(f'not responsible for checking mails {href}')
+                            print('new links founded', href)
+                else:
+                    if response.status_code == 403:
+                        self.deal_uom_sign_link(link, link_combo[1])
+                    else:
+                        self.deal_broken_link(link, link_combo[1], response.status_code, link_combo[2])
+                    print(f'status_code:{response.status_code}, broken_link:{link}, page source:{link_combo[1]}, associated_text:{link_combo[2]}')
+
+                    print(f'now the queue size is {self.web_links.qsize()}')
+            except Exception as e:
+                print(f'error fetch {link}, {str(e)}')
+            finally:
+                self.web_links.task_done()
+                self.counter -= 1
+                print(f'counter = {self.counter}')
+                print(f'remaining links number {self.web_links.qsize()}')
+
+    # help save time by filtering out broken link to reduce response time
+    def detect_links(self):
+        while True:
+            link_combo = self.web_links.get()
+            link = link_combo[0]
+
+            try:
+                print(f'detecting link {link}')
+                response = requests.get(link)
+                # if not broken, then put back to the queue
+                if response.status_code == 200:
+                    if link.startswith(self.baseurl):
+                        self.web_links.put(link_combo)
+                        self.counter += 1
+                    else:
+                        if self.web_links.qsize() == 0:
+                            print('finished')
+                            return
+                else:
+                    print(f'status_code:{response.status_code}, broken_link:{link}, page source:{link_combo[1]}')
+                    if response.status_code == 403:
+                        self.deal_uom_sign_link(link, link_combo[1])
+                    else:
+                        self.deal_broken_link(link, link_combo[1], response.status_code, link_combo[2])
+
+                    print(f'now the queue size is {self.web_links.qsize()}')
+            except Exception as e:
+                print(f'error fetch {link}, {str(e)}')
+            finally:
+                self.web_links.task_done()
+                self.counter -= 1
+
+                print(f'counter = {self.counter}')
+                print(f'remaining detected tasks{self.web_links.qsize()}')
+
+    def search_broken_links(self, baseurl):
+        self.put_url(baseurl)
+        thread_list = list()
+        for _ in range(20):
+            t = Thread(target=self.get_more_links)
+            thread_list.append(t)
+        for _ in range(20):
+            t = Thread(target=self.detect_links)
+            thread_list.append(t)
+        for t in thread_list:
+            t.daemon = True
+            t.start()
+        self.web_links.join()
+        print(self.keyword_links)
+        return self.broken_links
+    def search_keyword_links(self, baseurl, keyword):
+        self.put_keyword(keyword)
+        self.put_url(baseurl)
+        thread_list = list()
+        for _ in range(20):
+            t = Thread(target=self.get_more_links)
+            thread_list.append(t)
+        for _ in range(20):
+            t = Thread(target=self.detect_links)
+            thread_list.append(t)
+        for t in thread_list:
+            t.daemon = True
+            t.start()
+        self.web_links.join()
+        return self.keyword_links
+web_spider = Web_spider()
+results = web_spider.search_broken_links("https://sites.research.unimelb.edu.au/research-funding")
+print(results)
