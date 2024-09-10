7f26eb6be5d853f07d50900b23e7fed56fb94825
Zhonghao <zhosheng@student.unimelb.edu.au>
2024-09-11 05:33:27 +1000

    Remove result function

--- apps/search_link/urls.py
+++ apps/search_link/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.search_link, name='search_link'),  # Search page
     path('results/<uuid:job_id>/', views.results, name='results'),
     # path('user_left/', views.user_left, name='user_left'),
-    path('stop-job/<str:job_id>/', views.stop_job, name='stop_job'),
+    # path('stop-job/<str:job_id>/', views.stop_job, name='stop_job'),
     # path('job_status/<str:job_id>/', views.job_status, name='job_status'),
 ]
