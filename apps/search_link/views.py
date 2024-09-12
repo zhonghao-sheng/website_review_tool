07446f8ab0dc2872f54eaf8d9ea400993a2fda95
Engine55 <haxu4@student.unimelb.edu.au>
2024-09-12 12:44:44 +0800

    change search logic

--- apps/search_link/views.py
+++ apps/search_link/views.py
@@ -4,19 +4,18 @@ from threading import Thread
 from django.contrib.auth.decorators import login_required
 import requests
 from bs4 import BeautifulSoup
+import uuid
+import json
 import logging
+from django.http import JsonResponse
+
 import time
 from django.http import JsonResponse
-import os
-from django.views.decorators.csrf import csrf_exempt
+import time
 
-# Setting the expire time for the results in Redis to 3 minutes
-EXPIRE_TIME = 180
-# Setting the search time out to 10 seconds
-SEARCH_TIME_OUT = 30
 
-THREAD_NUM = 20
-
+def index(request):
+    return render(request, 'search.html')
 
 class Web_spider():
     def __init__(self):
@@ -31,33 +30,27 @@ class Web_spider():
         self.broken_links = list()
         self.keyword = 'Funding Partners'
         self.keyword_links = list()
-        self.job_id = None
 
-    def put_job_id(self, job_id):
-        self.job_id = job_id
 
     def put_url(self, baseurl):
         # [link, source page link, associated text]
         self.web_links.put([baseurl, None, None])
         self.counter += 1
         self.baseurl = baseurl
-
     def put_keyword(self, keyword):
         self.keyword = keyword
-
     def is_uom_sign_link(self, link):
         return self.baseurl in link
-    
     def add_uom_sign_link(self, link, source_link):
         self.UOM_sign_links.append({'url': link, 'source_link': source_link})
 
-    def write_uom_sign_link(self, link, source_link):
-        self.uom_sign_link_file.write(f'uom link:{link}, page source:{source_link}\n')
+    def write_uom_sign_link(self, link, associated_text, source_link):
+        self.uom_sign_link_file.write(f'uom link:{link}, associated_text:{associated_text},page source:{source_link}\n')
 
-    def deal_uom_sign_link(self, link, source_link):
+    def deal_uom_sign_link(self, link,associated_text, source_link):
         if self.is_uom_sign_link(link):
             self.add_uom_sign_link(link, source_link)
-            self.write_uom_sign_link(link, source_link)
+            self.write_uom_sign_link(link, associated_text, source_link)
     def add_broken_link(self, link, source_link, associated_text):
         self.broken_links.append({'url': link, 'source_link':
             source_link, 'associated_text': associated_text})
@@ -70,20 +63,15 @@ class Web_spider():
         self.add_broken_link(link, source_link, associated_text)
         self.write_broken_link(link, source_link, response_status, associated_text)
 
-    def get_more_links(self, current_job_id=None):
+    def get_more_links(self):
+
         while True:
-            if self.counter >= 200:
-                while not self.web_links.empty():
-                    self.web_links.get()
-                    self.web_links.task_done()
-                    self.counter -= 1
-                return
             link_combo = self.web_links.get()
 
             link = link_combo[0]
             try:
                 print(f'getting link {link}')
-                response = requests.get(link, timeout=SEARCH_TIME_OUT)
+                response = requests.get(link)
                 self.visited_or_about_to_visit.add(link)
                 if response.status_code == 200:
                     if not link.startswith(self.baseurl):
@@ -103,74 +91,51 @@ class Web_spider():
                                 self.visited_or_about_to_visit.add(href)
                                 self.web_links.put([href, link, text])
                                 self.counter += 1
-                                # Enqueue a new job for the found link
-                                # q.enqueue('process_link', href, link)
                             else:
                                 print(f'not responsible for checking mails {href}')
                             print('new links founded', href)
                 else:
                     if response.status_code == 403:
-                        self.deal_uom_sign_link(link, link_combo[1])
+                        self.deal_uom_sign_link(link, link_combo[2], link_combo[1])
                     else:
                         self.deal_broken_link(link, link_combo[1], response.status_code, link_combo[2])
                     print(f'status_code:{response.status_code}, broken_link:{link}, page source:{link_combo[1]}, associated_text:{link_combo[2]}')
 
-                    # print(f'now the queue size is {self.web_links.qsize()}')
-            except requests.exceptions.Timeout:
-                print(f'timeout fetching {link}')
-                self.deal_broken_link(link, link_combo[1], 'timeout', link_combo[2])
+                    print(f'now the queue size is {self.web_links.qsize()}')
             except Exception as e:
                 print(f'error fetch {link}, {str(e)}')
             finally:
                 self.web_links.task_done()
                 self.counter -= 1
-                print(f'Final counter = {self.counter}')
+                print(f'counter = {self.counter}')
                 print(f'remaining links number {self.web_links.qsize()}')
 
-                # Check if the queue is empty and counter is zero to break the loop
-                if self.web_links.qsize() == 0 and self.counter == 0:
-                    print('finished')
-                    break
-
     # help save time by filtering out broken link to reduce response time
-    def detect_links(self, current_job_id=None):
+    def detect_links(self):
         while True:
-            if self.counter >= 200:
-                while not self.web_links.empty():
-                    self.web_links.get()
-                    self.web_links.task_done()
-                    self.counter -= 1
-                return
             link_combo = self.web_links.get()
             link = link_combo[0]
 
             try:
                 print(f'detecting link {link}')
-                response = requests.get(link, timeout = SEARCH_TIME_OUT)
-
-                content_type = response.headers.get('Content-Type', '').lower()
-
-                # Check if the link is a valid download link
-                if 'application/' in content_type or 'octet-stream' in content_type:
-                    print(f'Valid download link detected: {link}')
-                    self.handle_download_link(link, link_combo[1], content_type)
-
+                response = requests.get(link)
                 # if not broken, then put back to the queue
-                elif response.status_code == 200:
+                if response.status_code == 200:
                     if link.startswith(self.baseurl):
                         self.web_links.put(link_combo)
                         self.counter += 1
                     else:
-                        if self.counter == 0:
+                        if self.web_links.qsize() == 0:
                             print('finished')
                             return
                 else:
                     print(f'status_code:{response.status_code}, broken_link:{link}, page source:{link_combo[1]}')
                     if response.status_code == 403:
-                        self.deal_uom_sign_link(link, link_combo[1])
+                        self.deal_uom_sign_link(link, link_combo[2],link_combo[1])
                     else:
                         self.deal_broken_link(link, link_combo[1], response.status_code, link_combo[2])
 
+                    print(f'now the queue size is {self.web_links.qsize()}')
             except Exception as e:
                 print(f'error fetch {link}, {str(e)}')
             finally:
@@ -178,61 +143,31 @@ class Web_spider():
                 self.counter -= 1
 
                 print(f'counter = {self.counter}')
-                # print(f'remaining detected tasks{self.web_links.qsize()}')
-
-
-                # Check if the queue is empty and counter is zero to break the loop
-                if self.web_links.qsize() == 0 and self.counter == 0:
-                    print('finished')
-                    break
-
-    # def handle_download_link(self, link, source_link, content_type):
-    #     # download the file
-    #     try:
-    #         response = requests.get(link, stream=True)
-    #         response.raise_for_status()  # Raise an exception for HTTP errors
-    #
-    #         # Extract the filename from the URL
-    #         filename = link.split('/')[-1]
-    #         download_path = os.path.join('downloads', filename)
-    #
-    #         # Ensure the downloads directory exists
-    #         os.makedirs(os.path.dirname(download_path), exist_ok=True)
-    #
-    #         # Write the file to the downloads directory
-    #         with open(download_path, 'wb') as file:
-    #             for chunk in response.iter_content(chunk_size=8192):
-    #                 file.write(chunk)
-    #
-    #         print(f'File downloaded: {download_path}')
-    #     except Exception as e:
-    #         logger.error(f'Failed to download file: {link}, error: {str(e)}')
-    #         # Log the failure to the broken links file
-    #         self.deal_broken_link(link, source_link, 'download_failed', str(e))
+                print(f'remaining detected tasks{self.web_links.qsize()}')
 
     def search_broken_links(self, baseurl):
         self.put_url(baseurl)
         thread_list = list()
-        for _ in range(THREAD_NUM):
+        for _ in range(20):
             t = Thread(target=self.get_more_links)
             thread_list.append(t)
-        for _ in range(THREAD_NUM):
+        for _ in range(20):
             t = Thread(target=self.detect_links)
             thread_list.append(t)
         for t in thread_list:
             t.daemon = True
             t.start()
         self.web_links.join()
+        print(self.keyword_links)
         return self.broken_links
-    
     def search_keyword_links(self, baseurl, keyword):
         self.put_keyword(keyword)
         self.put_url(baseurl)
         thread_list = list()
-        for _ in range(THREAD_NUM):
+        for _ in range(20):
             t = Thread(target=self.get_more_links)
             thread_list.append(t)
-        for _ in range(THREAD_NUM):
+        for _ in range(20):
             t = Thread(target=self.detect_links)
             thread_list.append(t)
         for t in thread_list:
