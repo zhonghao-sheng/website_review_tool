c68d048364e6afbf23f64c08988ccbd621aea9bd
Engine55 <haxu4@student.unimelb.edu.au>
2024-09-14 19:21:25 +0800

    s

--- website_review_tool/settings.py
+++ website_review_tool/settings.py
@@ -75,17 +75,17 @@ WSGI_APPLICATION = 'website_review_tool.wsgi.application'
 #
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-if 'RDS_HOSTNAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
+# if 'RDS_HOSTNAME' in os.environ:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ebdb',
+        'USER': 'ebroot',
+        'PASSWORD': 'nbmy43602',
+        'HOST': 'awseb-e-is39tew9m2-stack-awsebrdsdatabase-fg3qxeghnbjk.cdgamie849fo.ap-southeast-2.rds.amazonaws.com',
+        'PORT': 3306,
     }
+}
 
 # else:
 #     DATABASES = {
