a5e7e665032aaa291090da376b642aa453456a42
Engine55 <haxu4@student.unimelb.edu.au>
2024-09-18 19:49:05 +0800

    configure static

--- website_review_tool/settings.py
+++ website_review_tool/settings.py
@@ -143,9 +143,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static'),
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
