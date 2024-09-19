2165f845c159cae3196a7360a371145312fe3968
Engine55 <haxu4@student.unimelb.edu.au>
2024-09-19 21:29:19 +0800

    manually recover migration files

--- apps/login/models.py
+++ apps/login/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
-    username = models.CharField('Username', max_length=50, unique=True)
-    password = models.CharField('Password', max_length=16)
-    # email = models.CharField('Email', max_length=50, unique=True)
-    class Meta:
-        db_table = 'webtool_users'
-        verbose_name = 'user_management'
-        verbose_name_plural = verbose_name
+    pass
+    # username = models.CharField('Username', max_length=50, unique=True)
+    # password = models.CharField('Password', max_length=16)
+    # # email = models.CharField('Email', max_length=50, unique=True)
+    # class Meta:
+    #     db_table = 'webtool_users'
+    #     verbose_name = 'user_management'
+    #     verbose_name_plural = verbose_name
