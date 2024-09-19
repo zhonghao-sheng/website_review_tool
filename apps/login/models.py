c8e34a08039e6cac086d00ff0e3e1fde764abbe4
Engine55 <haxu4@student.unimelb.edu.au>
2024-09-19 21:31:30 +0800

    change model's inheritance class to AbstractUser

--- apps/login/models.py
+++ apps/login/models.py
@@ -2,11 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
-    pass
-    # username = models.CharField('Username', max_length=50, unique=True)
-    # password = models.CharField('Password', max_length=16)
-    # # email = models.CharField('Email', max_length=50, unique=True)
-    # class Meta:
-    #     db_table = 'webtool_users'
-    #     verbose_name = 'user_management'
-    #     verbose_name_plural = verbose_name
+    username = models.CharField('Username', max_length=50, unique=True)
+    password = models.CharField('Password', max_length=16)
+    # email = models.CharField('Email', max_length=50, unique=True)
+    class Meta:
+        db_table = 'webtool_users'
+        verbose_name = 'user_management'
+        verbose_name_plural = verbose_name
