d02c104522d54ea15d2b453273eaa8ad5d0a9518
Engine55 <haxu4@student.unimelb.edu.au>
2024-09-23 13:32:04 +0800

    merge sign up and email verfication

--- wildcard_practice.py
+++ wildcard_practice.py
@@ -1,20 +1,20 @@
-# from fnmatch import *
-# # currently fnmatch only supports * ? [seq] [!seq] in wildcard pattern, so need to write
-# # some manually
-# pattern = "fr%nka"
-# a = fnmatch("franka", pattern)
-#
-# # translate % and -  to * and ? in wildcard pattern into fnmatch recognizable
-# def translate_wildcard(pattern):
-#     pattern = pattern.replace('%', '*')
-#     pattern = pattern.replace('_', '?')
-#     return pattern
-# if a is False:
-#     pattern = translate_wildcard(pattern)
-#     print(fnmatch("franka", pattern))
-#     print
-# else:
-#     print(True)
+from fnmatch import *
+# currently fnmatch only supports * ? [seq] [!seq] in wildcard pattern, so need to write
+# some manually
+pattern = "fr%nka"
+a = fnmatch("franka", pattern)
+
+# translate % and -  to * and ? in wildcard pattern into fnmatch recognizable
+def translate_wildcard(pattern):
+    pattern = pattern.replace('%', '*')
+    pattern = pattern.replace('_', '?')
+    return pattern
+if a is False:
+    pattern = translate_wildcard(pattern)
+    print(fnmatch("franka", pattern))
+    print
+else:
+    print(True)
 
 from reportlab.lib.pagesizes import letter
 
