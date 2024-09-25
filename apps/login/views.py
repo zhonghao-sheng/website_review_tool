d53c02e1812e4b5622cd6e2a88105cdb9647de19
jazheng <jazheng@student.unimelb.edu.au>
2024-09-25 11:38:39 +1000

    username and password still not valid

--- apps/login/views.py
+++ apps/login/views.py
@@ -104,7 +104,7 @@ def forgot_password(request):
                 reset_password_email(request, user, email)
                 return redirect('login')
             else:
-                messages.error(request, f"No account found with the provided username and email.")
+                messages.error(request, f"No account found with the provided {username} and {email}.")
         else:
             messages.error(request, f"Username or password were invalid.")
     else:
